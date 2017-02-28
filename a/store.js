3b67f7e5af8d8936a1c75613657d8004a7b35f9c
Cameron Samuels <satchmosanders@gmail.com>
2017-02-27 19:19:48 -0600

    v1.1.0a32 - Changed overlay to be blue and not transparent, fixed rotation of overlay in multiplayer

--- a/store.js
+++ a/store.js
@@ -20,7 +20,7 @@ if (current.includes("firstTime")) {
 		unlock(current.replace("firstTime", ""));
 	} else {
 		showAlert('No silly! You can\'t get characters for free!');
-		id('unlockedPopupBtn').setAttribute('onclick', 'window.location="index.html?47"');	
+		id('unlockedPopupBtn').setAttribute('onclick', 'window.location="index.html?48"');	
 	}
 }
 else if (current != "firstTime") purchase(current);
