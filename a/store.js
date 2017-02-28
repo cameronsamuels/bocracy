5e24b468d67011f02cb785aee794efa045637ab9
Cameron Samuels <satchmosanders@gmail.com>
2017-02-28 15:26:38 -0600

    v1.1.0a35

--- a/store.js
+++ a/store.js
@@ -19,7 +19,7 @@ if (current.includes("firstTime")) {
 		unlock(current.replace("firstTime", ""));
 	} else {
 		showAlert('No silly! You can\'t get characters for free!');
-		id('unlockedPopupBtn').setAttribute('onclick', 'window.location="index.html?50"');	
+		id('unlockedPopupBtn').setAttribute('onclick', 'window.location="index.html?51"');	
 	}
 }
 else if (current != "firstTime") purchase(current);
