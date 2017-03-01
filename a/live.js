1e8b3ce55aff0d7b8a6e1582a0284b005e3b9a55
Cameron Samuels <satchmosanders@gmail.com>
2017-02-28 19:57:58 -0600

    v1.1.0a39 - Fixed legend margin bug again

--- a/live.js
+++ a/live.js
@@ -145,7 +145,7 @@ function unlockConfirmed(item) {
 }
 
 function purchase(item) {
-	setTimeout(function(){showConfirm("Buy a character from " + item + " for 500 redbacks?", "unlockConfirmed('" + item + "')", "window.location='index.html?54'")}, 500);
+	setTimeout(function(){showConfirm("Buy a character from " + item + " for 500 redbacks?", "unlockConfirmed('" + item + "')", "window.location='index.html?55'")}, 500);
 }
 
 function unlock(item) {
