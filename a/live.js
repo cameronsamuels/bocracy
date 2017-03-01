20693c8ca0176ab6a2a12c70b076c622c185179e
Cameron Samuels <satchmosanders@gmail.com>
2017-02-28 19:54:46 -0600

    v1.1.0a38 - Fixed legend being hidden under social buttons

--- a/live.js
+++ a/live.js
@@ -145,7 +145,7 @@ function unlockConfirmed(item) {
 }
 
 function purchase(item) {
-	setTimeout(function(){showConfirm("Buy a character from " + item + " for 500 redbacks?", "unlockConfirmed('" + item + "')", "window.location='index.html?53'")}, 500);
+	setTimeout(function(){showConfirm("Buy a character from " + item + " for 500 redbacks?", "unlockConfirmed('" + item + "')", "window.location='index.html?54'")}, 500);
 }
 
 function unlock(item) {
