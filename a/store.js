758078267b807ea35c1fb44feaddbfb126f8e3f8
Cameron Samuels <satchmosanders@gmail.com>
2017-03-05 10:14:48 -0600

    v1.1.0a61 - Added gradually transitioning health bars

--- a/store.js
+++ a/store.js
@@ -19,7 +19,7 @@ if (current.includes("firstTime")) {
 		unlock(current.replace("firstTime", ""));
 	} else {
 		showAlert('No silly! You can\'t get characters for free!');
-		id('unlockedPopupBtn').setAttribute('onclick', 'window.location="index.html?61"');	
+		id('unlockedPopupBtn').setAttribute('onclick', 'window.location="index.html?62"');	
 	}
 }
 else if (current != "firstTime") purchase(current);
