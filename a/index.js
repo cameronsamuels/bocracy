380b47642a4dadd6558e26fba67d86aa82bf365d
Cameron Samuels <satchmosanders@gmail.com>
2017-03-12 16:16:39 -0500

    Cleaned up landing page

--- a/index.js
+++
@@ -1,8 +0,0 @@
-function load() {
-	if (localStorage.loadedBOcracy == undefined) {
-		localStorage.loadedBOcracy = 'true';
-		window.location = 'battle.html#alinar';
-	}
-	id('wrapper').style.display = 'block';
-	id('loader').style.display = 'none';
-}
