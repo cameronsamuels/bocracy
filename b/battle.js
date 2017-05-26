b76e8507957f8eef39a9a795c391c181d34cf44f
SHEA\satch <satchmosanders@gmail.com>
2017-05-26 18:54:14 -0500

    cleaned up code, added acitus conflict, made future map areas to go to beta group

--- b/battle.js
+++ b/battle.js
@@ -123,6 +123,7 @@ function load() {
 		case "alinar": newStats = 'false'; badNames.url = "b"; goodNames.url = "a"; break;
 		case "eora": newStats = 'true'; badNames.url = "b"; goodNames.url = "a"; break;
 		case "cadrinal": newStats = 'false'; badNames.url = "b"; goodNames.url = "a"; break;
+		case "acitus": newStats = 'false'; badNames.url = "c"; goodNames.url = "a"; break;
 		default: newStats = 'false'; badNames.url = "b"; goodNames.url = "b";
 	}
 	if (newStats == 'true' && localStorage['has' + current.toString().charAt(0).toUpperCase() + current.toString().substring(1).replace('+', '')] == undefined) {
--- b/multiplayer.js
+++ b/multiplayer.js
@@ -67,6 +67,7 @@ function load() {
 		case "alinar": newStats = 'false'; badNames.url = "b"; goodNames.url = "a"; break;
 		case "eora": newStats = 'true'; badNames.url = "b"; goodNames.url = "a"; break;
 		case "cadrinal": newStats = 'false'; badNames.url = "b"; goodNames.url = "a"; break;
+		case "acitus": newStats = 'false'; badNames.url = "c"; goodNames.url = "a"; break;
 		default: newStats = 'false'; badNames.url = "b"; goodNames.url = "b";
 	}
 	a.name = badNames[current.replace('+', 'Boss')][Math.floor(Math.random() * badNames[current.replace('+', 'Boss')].length)];
