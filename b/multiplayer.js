dc5c59b779542c62e81b11e8aed1fe4dfa6d8440
SHEA\satch <satchmosanders@gmail.com>
2017-05-28 19:49:49 -0500

    updated multiplayer

--- b/multiplayer.js
+++ b/multiplayer.js
@@ -1,41 +1,36 @@
-var newStats, a = { name: '', health: 0, attack: 0, speed: 0 },
+var newStats, a = { name: '', health: 0, attack: 0 },
 b = { name: '', health: 0, attack: 0 },
-current = battles[Math.floor(Math.random() * battles.length)], newStats;
+current = battles[Math.floor(Math.random() * battles.length)];
 if (window.location.hash != '') current = window.location.hash.toString().replace('#', '');
 var game = { on : 'false',
 	refresh : { display : function() {
 		id('bHealthBar').style.width = (b.health / b.orig_health)*100 + '%';
 		id('aHealthBar').style.width = (a.health / a.orig_health)*100 + '%';
-		id('bButton').style.backgroundImage = 'url(https://bocracy.com/assets/' + goodNames.url + '/' + b.name.toString().replace('_', '-').replace('_', '-').replace('_', '-').replace('D', '.').replace('Boss', '') + '.png)';
-		id('aButton').style.backgroundImage = 'url(https://bocracy.com/assets/' + badNames.url + '/' + a.name.toString().replace('_', '-').replace('_', '-').replace('_', '-').replace('D', '.').replace('Boss', '') + '.png)';
 	}, all : function() {
 		game.refresh.display();
 		window.requestAnimationFrame(game.refresh.all);
 	}},	win : function(side) {
-		id('overlayText').innerHTML = '<div>WINNER: ' + side + '</div>';
+		id('overlayText').innerHTML = '<div>' + (side=='green'?'<--':'-->') + '</div>';
+		id('restartText').style.display = "none";
+		setTimeout(function(){id('restartText').style.display = "block"}, 750);
 		id('overlay').style.display = "block";
 		game.on = 'false';
 	}, attack : function(atk) {
 		if (game.on == 'true') { if (atk == 'green') {
 			a.health -= b.attack; a.health = Math.max(0, a.health); a.health = Math.min(a.orig_health, a.health);
-			var weapon  = 'sword';
-			if (newStats == 'true')	{ if (good[b.name].info[5] != undefined) weapon = good[b.name].info[5]; }
-			if (('url("img/' + weapon + '.png")') != id('bSword').style.backgroundImage) id('bSword').style.backgroundImage = 'url("img/' + weapon + '.png")';
-			id('bSword').style.display = "block"; id('bSword').style.WebkitAnimationName = "bSword";
+			id('bSword').style.display = "block";
 			id('bSword').style.animationName = "bSword";
-			setTimeout("id('bSword').style.display = 'none';id('bSword').style.WebkitAnimationName = '';id('bSword').style.animationName = '';", 100);
+			setTimeout("id('bSword').style.display = 'none';id('bSword').style.animationName = ''", 100);
 		} else if (atk == 'red') {
 			b.health -= a.attack; b.health = Math.max(0, b.health);
 			b.health = Math.min(b.orig_health, b.health);
-			var weapon  = 'sword';
-			if (newStats == 'true') { if (bad[a.name].info[5] != undefined) weapon = bad[a.name].info[5]; }
-			if (('url("img/' + weapon + '.png")') != id('aSword').style.backgroundImage) id('aSword').style.backgroundImage = 'url("img/' + weapon + '.png")';
 			id('aSword').style.display = "block";
-			id('aSword').style.WebkitAnimationName = "aSword";
 			id('aSword').style.animationName = "aSword";
-			setTimeout("id('aSword').style.display = 'none';id('aSword').style.WebkitAnimationName = '';id('aSword').style.animationName = '';", 100);
+			setTimeout("id('aSword').style.display = 'none';id('aSword').style.animationName = ''", 100);
 		}
 		if (b.health == 0 || a.health == 0) game.win(atk);
+		document.querySelector('#aHealth p').innerHTML = Math.round(a.health) + '/' + Math.round(a.orig_health);
+		document.querySelector('#bHealth p').innerHTML = Math.round(b.health) + '/' + Math.round(b.orig_health);
 	}}, heal : function(side) {
 		if (game.on == 'true') { if (side == 'green') {
 			b.health += b.heal;
@@ -49,13 +44,6 @@ var game = { on : 'false',
 	}}
 };
 function load() {
-	if (!isMobile.any()) id('computer').style.display = "block";
-	else { id('rotate').style.display = "block"; id('rotateInstructions').style.display = "block"; }
-	if (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)) {
-		id('computer').style.display = 'none';
-		id('rotate').style.display = "block";
-		id('rotateInstructions').style.display = "block";
-	}
 	if (!window.location.toString().includes('#')) current = battles[Math.floor(Math.random() * battles.length)];
 	switch (current.replace('+', '')) {
 		case "aonarchy": newStats = 'true'; badNames.url = "b"; goodNames.url = "a"; break;
@@ -74,7 +62,6 @@ function load() {
 	a.health = Math.min(Math.random() * 2250, 2000);
 	a.orig_health = a.health;
 	a.attack = Math.max(32, Math.random() * 40);
-	a.speed = Math.min(Math.random() * 1000, 500);
 	a.heal = Math.random() * 12;
 	b.name = goodNames[current.replace('+', 'Boss')][Math.floor(Math.random() * goodNames[current.replace('+', 'Boss')].length)];
 	while (b.name == a.name) { b.name = goodNames[current.replace('+', 'Boss')][Math.floor(Math.random() * goodNames[current.replace('+', 'Boss')].length)]; }
@@ -102,6 +89,31 @@ function load() {
 		id('bName').style.fontSize = "2vh";
 		id('aName').style.fontSize = "2vh";
 	}
+	id('bButton').style.backgroundImage = 'url(https://bocracy.com/assets/' + goodNames.url + '/' + b.name.toString().replace('_', '-').replace('_', '-').replace('_', '-').replace('D', '.').replace('Boss', '') + '.png)';
+	id('aButton').style.backgroundImage = 'url(https://bocracy.com/assets/' + badNames.url + '/' + a.name.toString().replace('_', '-').replace('_', '-').replace('_', '-').replace('D', '.').replace('Boss', '') + '.png)';
+	var weapon  = 'sword';
+	if (newStats == 'true') { if (bad[a.name].info[5] != undefined) weapon = bad[a.name].info[5] }
+	if (('url("img/' + weapon + '.png")') != id('aSword').style.backgroundImage) id('aSword').style.backgroundImage = 'url("img/' + weapon + '.png")';
+	var weapon  = 'sword';
+	if (newStats == 'true')	{ if (good[b.name].info[5] != undefined) weapon = good[b.name].info[5]; }
+	if (('url("img/' + weapon + '.png")') != id('bSword').style.backgroundImage) id('bSword').style.backgroundImage = 'url("img/' + weapon + '.png")';
+	document.querySelector('#aHealth p').innerHTML = Math.round(a.health) + '/' + Math.round(a.orig_health);
+	document.querySelector('#bHealth p').innerHTML = Math.round(b.health) + '/' + Math.round(b.orig_health);
+	var img = new Image();
+	img.onload = function() {
+		id('bSection').style.background = 'transparent';
+		id('aSection').style.background = 'transparent';
+		id('background').style.backgroundImage = 'url(img/' + current.replace('+', '') + '.svg)';
+		id('aButton').style.backgroundColor = 'transparent';
+		id('bButton').style.backgroundColor = 'transparent';
+	};
+	img.onerror = function() {
+		id('bSection').style.backgroundImage = '';
+		id('aSection').style.backgroundImage = '';
+		id('aButton').style.backgroundColor = '';
+		id('bButton').style.backgroundColor = '';
+	};
+	img.src = 'img/' + current.replace('+', '') + '.svg';
 }
 function restart() {
 	game.on = 'false';
