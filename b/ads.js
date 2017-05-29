2766b67b64f08bdcd6c26331209cb5b904678a83
SHEA\satch <satchmosanders@gmail.com>
2017-05-29 17:09:33 -0500

    cleaned up store and ads

--- b/ads.js
+++ b/ads.js
@@ -1,96 +1,46 @@
-	var videoContent = document.getElementById('contentElement');
-	var adDisplayContainer =
-	    new google.ima.AdDisplayContainer(
-	        document.getElementById('adContainer'),
-	        videoContent);
+var videoContent = id('contentElement');
+var adDisplayContainer = new google.ima.AdDisplayContainer(id('adContainer'),videoContent);
 function ad() {
-	document.getElementById('mainContainer').style.display = 'block';
+	id('mainContainer').style.display = 'block';
 	adDisplayContainer.initialize();
 	requestAds();
 }
-// Re-use this AdsLoader instance for the entire lifecycle of your page.
 var adsLoader = new google.ima.AdsLoader(adDisplayContainer);
-
-// Add event listeners
-adsLoader.addEventListener(
-    google.ima.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED,
-    onAdsManagerLoaded,
-    false);
-adsLoader.addEventListener(
-    google.ima.AdErrorEvent.Type.AD_ERROR,
-    onAdError,
-    false);
-
+adsLoader.addEventListener(google.ima.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED,onAdsManagerLoaded,false);
+adsLoader.addEventListener(google.ima.AdErrorEvent.Type.AD_ERROR,onAdError,false);
 function onAdError(adErrorEvent) {
-  // Handle the error logging and destroy the AdsManager
   console.log(adErrorEvent.getError());
   adsManager.destroy();
 }
-
-// An event listener to tell the SDK that our content video
-// is completed so the SDK can play any post-roll ads.
-var contentEndedListener = function() {
-	adsLoader.contentComplete();
-	localStorage.coins = parseFloat(localStorage.coins) + 250;
-	location.reload()};
+var contentEndedListener = function() {adsLoader.contentComplete();localStorage.coins = parseFloat(localStorage.coins) + 250;location.reload()};
 videoContent.onended = contentEndedListener;
-
-// Request video ads.
 var adsRequest = new google.ima.AdsRequest();
 adsRequest.adTagUrl = 'http://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-games-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on';
-
-// Specify the linear and nonlinear slot sizes. This helps the SDK to
-// select the correct creative if multiple are returned.
 adsRequest.linearAdSlotWidth = window.innerWidth;
 adsRequest.linearAdSlotHeight = window.innerHeight;
-adsRequest.nonLinearAdSlotWidth = 640;
-adsRequest.nonLinearAdSlotHeight = 150;
-
-var playButton = document.getElementById('playButton');
-playButton.addEventListener('click', ad);
-
+adsRequest.nonLinearAdSlotWidth = window.innerWidth;
+adsRequest.nonLinearAdSlotHeight = window.innerHeight;
+id('playButton').addEventListener('click', ad);
 function requestAds() {
   adsLoader.requestAds(adsRequest);
   videoContent.load();
 }
 function onAdsManagerLoaded(adsManagerLoadedEvent) {
-  // Get the ads manager.
-  adsManager = adsManagerLoadedEvent.getAdsManager(
-      videoContent);  // See API reference for contentPlayback
-
-  // Add listeners to the required events.
-  adsManager.addEventListener(
-      google.ima.AdErrorEvent.Type.AD_ERROR,
-      onAdError);
-  adsManager.addEventListener(
-      google.ima.AdEvent.Type.CONTENT_PAUSE_REQUESTED,
-      onContentPauseRequested);
-  adsManager.addEventListener(
-      google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED,
-      onContentResumeRequested);
-
+  adsManager = adsManagerLoadedEvent.getAdsManager(videoContent);
+  adsManager.addEventListener(google.ima.AdErrorEvent.Type.AD_ERROR,onAdError);
+  adsManager.addEventListener(google.ima.AdEvent.Type.CONTENT_PAUSE_REQUESTED,onContentPauseRequested);
+  adsManager.addEventListener(google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED,onContentResumeRequested);
   try {
-    // Initialize the ads manager. Ad rules playlist will start at this time.
     adsManager.init(window.innerWidth, window.innerWidth*(360/640), google.ima.ViewMode.NORMAL);
-    // Call start to show ads. Single video and overlay ads will
-    // start at this time; this call will be ignored for ad rules, as ad rules
-    // ads start when the adsManager is initialized.
     adsManager.start();
   } catch (adError) {
-    // An error may be thrown if there was a problem with the VAST response.
   }
 }
-
 function onContentPauseRequested() {
-  // This function is where you should setup UI for showing ads (e.g.
-  // display ad timer countdown, disable seeking, etc.)
   videoContent.removeEventListener('ended', contentEndedListener);
   videoContent.pause();
 }
-
 function onContentResumeRequested() {
-  // This function is where you should ensure that your UI is ready
-  // to play content.
   videoContent.addEventListener('ended', contentEndedListener);
   videoContent.play();
 }
