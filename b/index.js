58f63b4deb171afac34971f7bb9c18c527be1c4f
SHEA\satch <satchmosanders@gmail.com>
2017-06-01 15:57:55 -0500

    Made index be same for any orientation, made series selector be visual, more

--- b/index.js
+++ b/index.js
@@ -9,25 +9,28 @@ function battleground(e) {
 	document.querySelectorAll('#battleground div')[i].style.border= '1px transparent solid';
 	e.target.style.border = "1px white solid";
 	id('characters').innerHTML = "";
+	var html = "<div id='unselected'>";
 	for (i = 0; i < goodNames[b].length; i++) {
 		if (localStorage[goodNames[b][i]] == 'true') {
-			id('characters').innerHTML = id('characters').innerHTML + '<div>' + goodNames[b][i].replace('D', '.').replace('__', '-').replace('_', ' ').replace('_', ' ').replace('_', ' ') + '</div>';
+			html = html + '<div title="' + goodNames[b][i].replace('D', '.').replace('__', '-').replace('_', ' ').replace('_', ' ').replace('_', ' ') + '" style="background-image:url(https://bocracy.com/assets/a/' + goodNames[b][i].replace('.', 'D').replace('_', '-').replace('_', '-').replace('_', '-') + '.png)"></div>';
 		}
 	}
+	html = html + '</div><div id="selected"></div>';
+	id('characters').innerHTML = html;
 	id('startPlay').style.display = 'none';
 	id('notEnough').style.display = 'none';
 }
 }
 var cost;
 function character(e) {
-	if (e.target.id != 'characters') {
-	e.target.style.background=e.target.style.background?"":"#730005";
+	if (e.target.id != 'characters' && e.target.id != 'selected' && e.target.id != 'unselected') {
+	e.target.parentElement.id=='selected'?id('unselected').appendChild(e.target):id('selected').appendChild(e.target);
 	cost = 0;
-	for (i = 0; i < id('characters').querySelectorAll('[style*="background"]').length; i++) {
-		cost += Math.floor(good[id('characters').querySelectorAll('[style*="background"]')[i].innerHTML.replace('.', 'D').replace('-', '__').replace(' ', '_').replace(' ', '_').replace(' ', '_')].info[2] *  0.03);
+	for (i = 0; i < id('characters').querySelectorAll('#selected div').length; i++) {
+		cost += Math.floor(good[id('characters').querySelectorAll('#selected div')[i].title.replace('.', 'D').replace('-', '__').replace(' ', '_').replace(' ', '_').replace(' ', '_')].info[2] *  0.03);
 		cost += 5;
 	}
-	if (id('characters').querySelectorAll('[style*="background"]').length > 0) {
+	if (id('characters').querySelectorAll('#selected div').length > 0) {
 		id('startPlay').style.display = 'block';
 		id('startPlay').innerHTML = 'Play (' + cost + ')';
 	}
@@ -36,14 +39,14 @@ function character(e) {
 		id('notEnough').style.display = 'block';
 		id('notEnough').innerHTML = "Not enough (" + localStorage.coins + "/" + cost + ")";
 	} else id('notEnough').style.display = 'none';
-	if (id('characters').querySelectorAll('[style*="background"]').length == 0) id('startPlay').style.display = 'none';
+	if (id('characters').querySelectorAll('#selected div').length == 0) id('startPlay').style.display = 'none';
 	}
 }
 function startPlay() {
 	if (localStorage.coins >= cost) localStorage.coins -= cost;
-	var characters = id('characters').querySelectorAll('[style*="background"]');
+	var characters = id('characters').querySelectorAll('#selected div');
 	for (i = 0; i < characters.length; i++) {
-		localStorage['b' + (i+1)] = characters[i].innerHTML.replace('.', 'D').replace('-', '__').replace(' ', '_').replace(' ', '_').replace(' ', '_');
+		localStorage['b' + (i+1)] = characters[i].title.replace('.', 'D').replace('-', '__').replace(' ', '_').replace(' ', '_').replace(' ', '_');
 	}
 	localStorage.sc = characters.length;
 	location = "battle.html#series" + b;
