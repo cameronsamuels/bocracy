0e74ca2bf20a52becdfc9355490ceba6a6e18895
Cameron Samuels <me@cameronsamuels.com>
2017-07-22 13:10:58 -0500

    Beautified ads.js

--- b/ads.js
+++ b/ads.js
@@ -1,13 +1,17 @@
-var vc = $('contentElement'), adc = new google.ima.AdDisplayContainer($('adContainer'),vc),
+var vc = $('contentElement'), adc = new google.ima.AdDisplayContainer($('adContainer'), vc),
 al = new google.ima.AdsLoader(adc), ar = new google.ima.AdsRequest(),
-cel = function() {al.contentComplete();ls.coins = parseFloat(ls.coins) + 250;location.reload()};
+cel = function() {
+    al.contentComplete();
+    ls.coins = parseFloat(ls.coins) + 250;
+    location.reload()
+};
 function ad() {
-	$('mainContainer').style.display = 'block';
-	adc.initialize();
-	ra();
+  $('mainContainer').style.display = 'block';
+  adc.initialize();
+  ra();
 }
-al.addEventListener(google.ima.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED,aml),
-al.addEventListener(google.ima.AdErrorEvent.Type.AD_ERROR,oe), vc.onended = cel;
+al.addEventListener(google.ima.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED, aml),
+al.addEventListener(google.ima.AdErrorEvent.Type.AD_ERROR, oe), vc.onended = cel;
 function oe(e) {
   console.log(e.getError());
   am.destroy();
@@ -21,11 +25,11 @@ function ra() {
 }
 function aml(e) {
   am = e.getAdsManager(vc);
-  am.addEventListener(google.ima.AdErrorEvent.Type.AD_ERROR,oe),
-  am.addEventListener(google.ima.AdEvent.Type.CONTENT_PAUSE_REQUESTED,cp),
-  am.addEventListener(google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED,cr);
+  am.addEventListener(google.ima.AdErrorEvent.Type.AD_ERROR, oe),
+    am.addEventListener(google.ima.AdEvent.Type.CONTENT_PAUSE_REQUESTED, cp),
+    am.addEventListener(google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED, cr);
   try {
-    am.init(innerWidth, innerWidth*(360/640), google.ima.ViewMode.NORMAL);
+    am.init(innerWidth, innerWidth * (360 / 640), google.ima.ViewMode.NORMAL);
     am.start();
   } catch (e) {}
 }
