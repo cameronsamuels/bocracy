92a712216d25152a9bd10f0ff5b85f55a1218cee
Cameron Samuels <me@cameronsamuels.com>
2017-11-20 20:43:22 -0600

    (improvement)Changed mob() to isMobile (#139)
    I made mob() not a function and just a variable. This will speed up browser and computer proccessing.

--- b/index/index.js
+++ b/index/index.js
@@ -1,3 +1,3 @@
-if (!mob())
+if (!isMobile)
 	document.querySelectorAll('main div')[2].innerHTML = "BETA",
 	document.querySelectorAll('main div')[2].setAttribute('ontouchend', "location='https://bocracy.com/betatest'");
