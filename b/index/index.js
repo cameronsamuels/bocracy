1bdd772a0e220a2ef960955874a05b0d6368b723
Cameron Samuels <me@cameronsamuels.com>
2017-12-10 19:31:24 -0600

    (issue)Redesigned landing page
    I redesigned the landing page to have a new background (designed by @ethanchoo) and a new layout.
    
    This improves the game because:
    - A better looking background
    - A better looking page
    - A more responsive page
    - Cleaner code (100% cleaned and js with comments)

--- b/index/index.js
+++ b/index/index.js
@@ -1,3 +1,51 @@
-if (!isMobile)
-	document.querySelectorAll('main div')[2].innerHTML = "BETA",
-	document.querySelectorAll('main div')[2].setAttribute('ontouchend', "location='https://bocracy.com/betatest'");
+// Define the play button
+var playButton = document.querySelector("main>div"),
+// Define the bottom buttons container
+buttonsContainer = document.querySelector("section"),
+// Define the buttons to create
+buttons = ["endless:endless", "pvp:multiplayer", "store:store", "credits:credits"],
+// Define if a mobile device
+isMobile = navigator.userAgent.match(/Android|IEMobile|iPhone|iPad|iPod/i);
+// Add a click event to the play button
+playButton.addEventListener(isMobile?"touchend":"click", function() {
+	// Load the battle page
+	location = "battle/index.html";
+});
+// For every button needed to create
+for (i = 0; i < buttons.length; i++) {
+	// Create the button element
+	var button = document.createElement("div");
+	// Add text to the button
+	button.textContent = buttons[i].split(":")[0];
+	// Add a click event to the button
+	button.addEventListener(isMobile?"touchend":"click", function(e) {
+		// Searches for the clicked button
+		function buttonSearch(i) {
+			// Returns true if this is the button clicked
+			return i.includes(e.target.textContent);
+		}
+		// Load the page the button needs
+		location = buttons.find(buttonSearch).split(":")[1] + "/index.html";
+	});
+	// Add the button to the page
+	buttonsContainer.appendChild(button);
+}
+// If on a desktop computer, hide the pvp button
+if (!isMobile) document.querySelector('section div:nth-child(2)').style.display = "none";
+// If Internet Explorer
+if (document.documentMode == 11 || document.documentMode == 10) {
+	// Show a popup alerting the user
+	alert("This browser is not supported by b-Ocracy. Download Google Chrome");
+	// Redirect to download Chrome
+	location = "https://google.com/chrome/browser/desktop";
+}
+// Add page load event
+document.addEventListener('DOMContentLoaded', function() {
+	// Execute after 1 second
+	setTimeout(function() {
+		// Hide the loading logo
+		document.querySelector("body>img").style.display = "none";
+	}, 1000);
+});
+// Disable touch moving (scrolling)
+document.addEventListener("touchmove", function(e){ e.preventDefault() });
